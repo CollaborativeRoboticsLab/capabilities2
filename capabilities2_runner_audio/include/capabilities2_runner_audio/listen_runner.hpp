e13c57de7384cd0f0659a607510af924f9cdf538
BuddhiGamage <buddhigamage217@gmail.com>
2024-09-16 13:27:35 +1000

    listen runner initialized

--- capabilities2_runner_audio/include/capabilities2_runner_audio/listen_runner.hpp
+++ capabilities2_runner_audio/include/capabilities2_runner_audio/listen_runner.hpp
@@ -1,24 +1,68 @@
 #pragma once
 
+#include <tinyxml2.h>
 #include <capabilities2_runner/action_runner.hpp>
+#include <hri_audio_msgs/action/speech_to_text.hpp>
+#include <rclcpp/rclcpp.hpp>
+#include <rclcpp_action/rclcpp_action.hpp>
+#include <thread>
 
-namespace capabilities2_runner_audio
+namespace capabilities2_runner
 {
-class ListenRunner : public capabilities2_runner::ActionRunner<audio_msgs::action::STT>
+
+/**
+ * @brief action runner base class
+ *
+ * Class to run VoiceListener action based capability
+ *
+ */
+class VoiceListenerRunner : public ActionRunner<hri_audio_msgs::action::speech_to_text>
 {
 public:
-  ListenRunner(/* args */);
-  ~ListenRunner();
+  VoiceListenerRunner() : ActionRunner()
+  {
+  }
 
-private:
+  /**
+   * @brief Starter function for starting the action runner
+   *
+   * @param node shared pointer to the capabilities node. Allows to use ros node related functionalities
+   * @param run_config runner configuration loaded from the yaml file
+   * @param on_started function pointer to trigger at the start of the action client in the runner
+   * @param on_terminated function pointer to trigger at the termination of the action client in the runner
+   */
+  virtual void start(rclcpp::Node::SharedPtr node, const runner_opts& run_config,
+                     std::function<void(const std::string&)> on_started = nullptr,
+                     std::function<void(const std::string&)> on_terminated = nullptr) override
+  {
+    init_action(node, run_config, "speech_to_text", on_started, on_terminated);
+  }
+
+  /**
+   * @brief trigger the runner
+   *
+   @param parameters XMLElement that contains parameters in the format '<speech_to_text recognized_text='$value'/>'
+   */
+  virtual void trigger(std::shared_ptr<tinyxml2::XMLElement> parameters = nullptr)
+  {
+    tinyxml2::XMLElement* parametersElement = parameters->FirstChildElement("speech_to_text");
+
+    hri_audio_msgs::action::SpeechToText::Goal goal_msg;
+
+    
+
+
+    // launch runner using action client
+    action_client_->async_send_goal(goal_msg, send_goal_options_);
+  }
+
+  
+
+protected:
+  std::string global_frame_;     /**The global frame of the robot*/
+  std::string robot_base_frame_; /**The frame of the robot base*/
+
+  double x, y; /**Coordinate frame parameters*/
 };
 
-ListenRunner::ListenRunner(/* args */)
-{
-}
-
-ListenRunner::~ListenRunner()
-{
-}
-
-}  // namespace capabilities2_runner_audio
+}  // namespace capabilities2_runner
