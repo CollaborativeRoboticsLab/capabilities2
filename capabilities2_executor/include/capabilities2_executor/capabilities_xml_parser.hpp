84f7f48d164bd03217a56c66409e3deaf6109736
Kalana Ratnayake <kalanaratnayake95@gmail.com>
2024-09-27 12:51:55 +1000

    added comments

--- capabilities2_executor/include/capabilities2_executor/capabilities_xml_parser.hpp
+++ capabilities2_executor/include/capabilities2_executor/capabilities_xml_parser.hpp
@@ -5,6 +5,18 @@
 namespace capabilities2_xml_parser
 {
 	/**
+	 * @brief extract elements related plan
+	 *
+	 * @param document XML document to extract plan from
+	 *
+	 * @return plan in the form of tinyxml2::XMLElement*
+	 */
+	tinyxml2::XMLElement* get_plan(tinyxml2::XMLDocument &document)
+	{
+		return document.FirstChildElement("Plan");
+	}
+
+	/**
 	 * @brief search a string in a vector of strings
 	 *
 	 * @param list vector of strings to be searched
@@ -29,6 +41,13 @@ namespace capabilities2_xml_parser
 		return (element == element->Parent()->LastChildElement());
 	}
 
+	/**
+	 * @brief check if the xml document has valid plan tags
+	 *
+	 * @param document XML document in question
+	 *
+	 * @return `true` if its valid and `false` otherwise
+	 */
 	bool check_plan_tag(tinyxml2::XMLDocument &document)
 	{
 		std::string plan_tag(document.FirstChildElement()->Name());
@@ -39,8 +58,18 @@ namespace capabilities2_xml_parser
 			return false;
 	}
 
+	/**
+	 * @brief check the plan for invalid/unsupported control and event tags
+	 * uses recursive approach to go through the plan
+	 *
+	 * @param element XML Element to be evaluated
+	 * @param events_list std::string containing valid event tags
+	 * @param control_list std::string containing valid control tags
+	 *
+	 * @return `true` if element valid and supported and `false` otherwise
+	 */
 	bool check_tags(tinyxml2::XMLElement *element,
-					std::vector<std::string> &actions_list,
+					std::vector<std::string> &events_list,
 					std::vector<std::string> &control_list)
 	{
 		const char **name;
@@ -50,19 +79,19 @@ namespace capabilities2_xml_parser
 		std::string typetag(element->Name());
 		std::string nametag(*name);
 
-		bool hasChildren = !element->NoChildren();
-		bool hasSiblings = !capabilities2_xml_parser::isLastElement(element);
+		bool hasChildren 	= !element->NoChildren();
+		bool hasSiblings 	= !capabilities2_xml_parser::isLastElement(element);
 		bool foundInControl = capabilities2_xml_parser::search(control_list, nametag);
-		bool foundInActions = capabilities2_xml_parser::search(actions_list, nametag);
-		bool returnValue = true;
+		bool foundInEvents  = capabilities2_xml_parser::search(events_list, nametag);
+		bool returnValue 	= true;
 
 		if (typetag == "Control")
 		{
 			if (foundInControl and hasChildren)
-				returnValue = returnValue and capabilities2_xml_parser::check_tags(element->FirstChildElement(), actions_list, control_list);
+				returnValue = returnValue and capabilities2_xml_parser::check_tags(element->FirstChildElement(), events_list, control_list);
 
 			if (foundInControl and hasSiblings)
-				returnValue = returnValue and capabilities2_xml_parser::check_tags(element->NextSiblingElement(), actions_list, control_list);
+				returnValue = returnValue and capabilities2_xml_parser::check_tags(element->NextSiblingElement(), events_list, control_list);
 
 			if (foundInControl and !hasSiblings)
 				returnValue = returnValue;
@@ -72,13 +101,13 @@ namespace capabilities2_xml_parser
 		}
 		else if (typetag == "Event")
 		{
-			if (foundInActions and hasSiblings)
-				returnValue = returnValue and capabilities2_xml_parser::check_tags(element->NextSiblingElement(), actions_list, control_list);
+			if (foundInEvents and hasSiblings)
+				returnValue = returnValue and capabilities2_xml_parser::check_tags(element->NextSiblingElement(), events_list, control_list);
 
-			if (foundInActions and !hasSiblings)
+			if (foundInEvents and !hasSiblings)
 				returnValue = returnValue;
 
-			if (!foundInActions)
+			if (!foundInEvents)
 				return false;
 		}
 		else
@@ -89,9 +118,6 @@ namespace capabilities2_xml_parser
 		return returnValue;
 	}
 
-	tinyxml2::XMLElement *get_plan(tinyxml2::XMLDocument &document)
-	{
-		return document.FirstChildElement("Plan");
-	}
+	
 
 } // namespace capabilities2_xml_parser
