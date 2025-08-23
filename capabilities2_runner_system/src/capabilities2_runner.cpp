27893bc493b82d710df8724b1d0d5167a1031448
Kalana Ratnayake <kalanaratnayake95@gmail.com>
2025-08-23 17:20:56 +1000

    fixes to match code refactoring

--- capabilities2_runner_system/src/capabilities2_runner.cpp
+++ capabilities2_runner_system/src/capabilities2_runner.cpp
@@ -1,10 +1,8 @@
 #include <pluginlib/class_list_macros.hpp>
 #include <capabilities2_runner/runner_base.hpp>
-#include <capabilities2_runner_system/completion_runner.hpp>
 #include <capabilities2_runner_system/input_multiplex_all_runner.hpp>
 #include <capabilities2_runner_system/input_multiplex_any_runner.hpp>
 
 // register runner plugins
 PLUGINLIB_EXPORT_CLASS(capabilities2_runner::InputMultiplexAllRunner, capabilities2_runner::RunnerBase);
 PLUGINLIB_EXPORT_CLASS(capabilities2_runner::InputMultiplexAnyRunner, capabilities2_runner::RunnerBase);
-PLUGINLIB_EXPORT_CLASS(capabilities2_runner::CompletionRunner, capabilities2_runner::RunnerBase)
