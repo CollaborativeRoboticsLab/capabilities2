bc4b3009de9d8b0245ad2560333bc06a5df5e091
mik-p <mppritchard3@hotmail.com>
2026-01-14 10:05:05 +0000

    update build with event package, remove external util and log packages

--- capabilities2_runner/src/multiplex_any_runner.cpp
+++ capabilities2_runner/src/multiplex_any_runner.cpp
@@ -1,6 +1,6 @@
 #include <pluginlib/class_list_macros.hpp>
 #include <capabilities2_runner/runner_base.hpp>
-#include <capabilities2_runner_system/input_multiplex_any_runner.hpp>
+#include <capabilities2_runner/input_multiplex_any_runner.hpp>
 
 // register runner plugins
 PLUGINLIB_EXPORT_CLASS(capabilities2_runner::InputMultiplexAllRunner, capabilities2_runner::RunnerBase);
